b67282bf38cea678bab6835048332ce1df76e86c
lk101101 <55768135+lk101101@users.noreply.github.com>
2021-01-01 20:56:54 -0500

    Pack.hpp - Pack header file

---
+++ Pack.hpp
@@ -0,0 +1,50 @@
+#ifndef Pack_hpp
+#define Pack_hpp
+#include <stdio.h>
+#include "Card.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+
+using namespace std;
+
+class Pack {
+public:
+    // EFFECTS: initializes all 52 cards into pack
+    Pack();
+    
+    // EFFECTS: returns size of pack as an int
+    int get_size();
+    
+    // EFFECTS: returns true if pack size is equal to 52; returns false otherwise
+    bool is_full();
+    
+    //EFFECTS: returns true if pack size is equal to 0; returns false otherwise
+    bool is_empty();
+    
+    // REQUIRES: non-empty pack
+    // EFFECTS: shuffles cards in a random order
+    void shuffle_pack();
+    
+    // REQUIRES: non-empty pack
+    // EFFECTS: deals one card randomly from pack
+    Card deal();
+    
+    // REQUIRES: non-empty pack
+    // MODIFIES: Pack vector (decreases size + removes last card)
+    // EFFECTS: removes last card in Pack
+    void remove_card();
+    
+    // EFFECTS: prints all cards in pack
+    void print_pack();
+        
+private:
+    // pack of cards of type Card
+    vector<Card> pack;
+    static const int FULL_PACK_SIZE = 52;
+};
+#endif /* Pack_hpp */
